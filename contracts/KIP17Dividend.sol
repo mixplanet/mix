ac511b58bd6da81ca32ffb92a027a89e2f136446
Hanul <hanul@hanul.me>
2021-09-30 23:16:08 +0900

    KIP17Dividend 개발

---
+++ contracts/KIP17Dividend.sol
@@ -0,0 +1,93 @@
+pragma solidity ^0.5.6;
+
+import "./klaytn-contracts/token/KIP17/IKIP17Enumerable.sol";
+import "./klaytn-contracts/math/SafeMath.sol";
+import "./libraries/SignedSafeMath.sol";
+import "./interfaces/IKIP17Dividend.sol";
+import "./interfaces/IMixEmitter.sol";
+import "./interfaces/IMix.sol";
+
+contract KIP17Dividend is IKIP17Dividend {
+    using SafeMath for uint256;
+    using SignedSafeMath for int256;
+
+    IMixEmitter private mixEmitter;
+    IMix private mix;
+    uint256 private pid;
+    IKIP17Enumerable private nft;
+
+    constructor(
+        IMixEmitter _mixEmitter,
+        IMix _mix,
+        uint256 _pid,
+        IKIP17Enumerable _nft
+    ) public {
+        mixEmitter = _mixEmitter;
+        mix = _mix;
+        pid = _pid;
+        nft = _nft;
+    }
+
+    uint256 internal currentBalance = 0;
+
+    uint256 constant internal pointsMultiplier = 2**128;
+    uint256 internal pointsPerShare = 0;
+    mapping(uint256 => int256) internal pointsCorrection;
+    mapping(uint256 => uint256) internal claimed;
+
+    function updateBalance() internal {
+        uint256 totalSupply = nft.totalSupply();
+        if (totalSupply > 0) {
+            mixEmitter.updatePool(pid);
+            uint256 balance = mix.balanceOf(address(this));
+            uint256 value = balance.sub(currentBalance);
+            if (value > 0) {
+                pointsPerShare = pointsPerShare.add(value.mul(pointsMultiplier).div(totalSupply));
+                emit Distribute(msg.sender, value);
+            }
+            currentBalance = balance;
+        }
+    }
+
+    function claimedOf(uint256 id) public view returns (uint256) {
+        return claimed[id];
+    }
+
+    function accumulativeOf(uint256 id) public view returns (uint256) {
+        uint256 _pointsPerShare = pointsPerShare;
+        uint256 totalSupply = nft.totalSupply();
+        if (totalSupply > 0) {
+            uint256 balance = mixEmitter.pendingMix(pid).add(mix.balanceOf(address(this)));
+            uint256 value = balance.sub(currentBalance);
+            if (value > 0) {
+                _pointsPerShare = _pointsPerShare.add(value.mul(pointsMultiplier).div(totalSupply));
+            }
+            return uint256(int256(_pointsPerShare).add(pointsCorrection[id])).div(pointsMultiplier);
+        }
+        return 0;
+    }
+
+    function claimableOf(uint256 id) external view returns (uint256) {
+        return accumulativeOf(id).sub(claimed[id]);
+    }
+
+    function _accumulativeOf(uint256 id) internal view returns (uint256) {
+        return uint256(int256(pointsPerShare).add(pointsCorrection[id])).div(pointsMultiplier);
+    }
+
+    function _claimableOf(uint256 id) internal view returns (uint256) {
+        return _accumulativeOf(id).sub(claimed[id]);
+    }
+
+    function claim(uint256 id) external {
+        require(nft.ownerOf(id) == msg.sender);
+        updateBalance();
+        uint256 claimable = _claimableOf(id);
+        if (claimable > 0) {
+            claimed[id] = claimed[id].add(claimable);
+            emit Claim(id, claimable);
+            mix.transfer(msg.sender, claimable);
+            currentBalance = currentBalance.sub(claimable);
+        }
+    }
+}
--- contracts/interfaces/IKIP17Dividend.sol
+++ contracts/interfaces/IKIP17Dividend.sol
@@ -1,4 +1,12 @@
 pragma solidity ^0.5.6;
 
 interface IKIP17Dividend {
+
+    event Distribute(address indexed by, uint256 distributed);
+    event Claim(uint256 indexed id, uint256 claimed);
+
+    function accumulativeOf(uint256 id) external view returns (uint256);
+    function claimedOf(uint256 id) external view returns (uint256);
+    function claimableOf(uint256 id) external view returns (uint256);
+    function claim(uint256 id) external;
 }
