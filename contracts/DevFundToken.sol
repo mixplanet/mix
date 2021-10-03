66b976ab27390e7e557d71dcf22985262df271a4
Hanul <hanul@hanul.me>
2021-10-03 17:40:13 +0900

    Burn 풀 추가

--- contracts/DevFundToken.sol
+++ contracts/DevFundToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.6;
 import "./klaytn-contracts/token/KIP7/KIP7.sol";
 import "./klaytn-contracts/token/KIP7/KIP7Metadata.sol";
 
-contract DevFundToken is KIP7, KIP7Metadata("DSC Dev Fund Token", "DEV", 18) {
+contract DevFundToken is KIP7, KIP7Metadata("DSC Dev Fund Token", "DSCDEV", 18) {
 
     constructor() public {
         _mint(msg.sender, 100 * 1e18);
---
+++ contracts/interfaces/IBurnPool.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.5.6;
+
+interface IBurnPool {
+    function burn(uint256 amount) external;
+}
